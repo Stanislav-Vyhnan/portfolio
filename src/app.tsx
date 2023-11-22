0b65066842f5e77799c8a7e8b85539d6fe31043b
Stanislav Vyhnan <stas.vignan@gmail.com>
2023-11-22 15:22:49 +0200

    feat: added favicon and title

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from 'react';
+
 import Navigation from './navigation';
 
-const App = () => <Navigation />;
+const App = () => {
+  useEffect(() => {
+    console.log('Version v.1.0.0');
+  }, []);
+
+  return <Navigation />;
+};
 
 export default App;
